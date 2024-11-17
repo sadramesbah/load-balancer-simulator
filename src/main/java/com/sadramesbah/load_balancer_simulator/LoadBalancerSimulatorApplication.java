b93fc2859f58046204b26a794d8ad71f728ed2c3
SadraMesbah <sadra.mesbah@gmail.com>
2024-11-16 22:25:08 -0500

    Create spring boot application

---
+++ src/main/java/com/sadramesbah/load_balancer_simulator/LoadBalancerSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.sadramesbah.load_balancer_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancerSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancerSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadramesbah/load_balancer_simulator/LoadBalancerSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadramesbah.load_balancer_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancerSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
